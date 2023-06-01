672089eb776d06e6b67aceeb8504030b7fad678a
Malkishara <isurumalkishara97@gmail.com>
2023-06-01 10:49:52 +0530

    add, update and delete items on item page

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
 import CategoryService from '../services/CategoryService';
 import ItemService from '../services/ItemService';
+import AddItemPopup from '../components/AddItemPopup';
+import SuccessComponent from '../components/SuccessComponent';
+import DeletePopup from '../components/DeletePopup';
+import UpdateItemPopup from '../components/UpdateItemPopup';
 import '../styles/ItemsPage.css';
 
 function ItemPage() {
   const [items, setItems] = useState([]);
   const [categoryName,setCategoryName]=useState('');
+  const [isAddPopupOpen, setAddPopupOpen] = useState(false);
+  const [isSuccessPopupOpen,setSuccessPopupOpen]=useState(false);
+  const [isDeletePopupOpen, setDeletePopupOpen] = useState(false);
+  const [itemId,setItemId]=useState();
+  const [isUpdatePopupOpen,setUpdatePopupOpen]=useState(false);
+  const [itemToUpdate,setItemToUpdate]=useState(null);
+  
 
-  const { cid } = useParams();
+  const { id,mid,cid } = useParams();
+
+  const navigate=useNavigate();
 
   useEffect(() => {
     getCategoryById();
@@ -41,7 +58,118 @@ function ItemPage() {
       });
   };
 
+//add popup open
+  const handleAddItem=()=>{
+    console.warn("add popup open");
+ setAddPopupOpen(true);
+}
 
+// Close add popup
+const closeAddPopup = () => {
+setAddPopupOpen(false);
+};
+
+  //add item
+  const add=(name,price,bannerImg)=>{
+    const businessId=sessionStorage.getItem('userId');
+    const data={
+      "business":businessId,
+      "branch":id,
+      "menu":mid,
+      "category":cid,
+      "subCategory":null,
+      "name":name,
+      "price":price,
+      "bannerImg":bannerImg
+    }
+
+    console.warn(data);
+    ItemService.addItem(data).then(res=>{
+      console.warn(res.data);
+      if(res.data.result==true){
+        setAddPopupOpen(false);
+      setSuccessPopupOpen(true);
+      
+     
+    }
+    })
+
+  }
+
+  //close success popup
+const closeSuccessPopup=()=>{
+  setSuccessPopupOpen(false);
+  navigate('/branch/'+id+'/menu/'+mid+'/category/'+cid+'/item');
+}
+
+//update item
+const handleEditItem=(item)=>{
+setItemToUpdate(item);
+setUpdatePopupOpen(true);
+}
+
+ // Update type
+ const update = (name, price, bannerImg) => {
+  console.log('Updating item with ID: ' + itemToUpdate.id);
+  const businessId=sessionStorage.getItem('userId');
+  const data={
+    "id":itemToUpdate.id,
+    "business":businessId,
+    "branch":id,
+    "menu":mid,
+    "category":cid,
+    "subCategory":null,
+    "name":name,
+    "price":price,
+    "bannerImg":bannerImg
+     }
+  
+     console.warn(data);
+  // Perform the update action 
+  ItemService.updateItem(id,data).then(res=>{
+      console.log(res.data);
+      if(res.data.result==true){
+          setUpdatePopupOpen(false);
+        setSuccessPopupOpen(true);
+        
+       
+      }
+    })
+  // Close the popup
+  setUpdatePopupOpen(false);
+ 
+};
+
+// ...
+
+// Close update popup
+const closeUpdatePopup = () => {
+  setUpdatePopupOpen(false);
+};
+
+//delete item
+const handleDeleteItem=()=>{
+  console.warn("open delete popup");
+  setItemId(id);
+  setDeletePopupOpen(true);
+}
+
+// Confirm delete
+const confirmDelete = () => {
+  // Perform the delete action
+  // You can use the `vacancyId` variable here to perform the delete action
+  console.log('Deleting item with ID: ' + itemId);
+  // Close the popup
+  setDeletePopupOpen(false);
+  ItemService.deleteItem(itemId).then(res=>{
+    console.log(res.data);
+    if(res.data.result==true){
+      setSuccessPopupOpen(true);
+     
+    }
+  })
+  
+};
   return (
     <div className='item'>
       <div className='item-content'>
@@ -50,11 +178,18 @@ function ItemPage() {
                 <h1>{categoryName}</h1>
             </div>
         </div>
+        <div className='row'>
+            <div className='col' style={{ textAlign:'right' }}>
+            <IconButton onClick={() => handleAddItem()}>
+                      <AddBoxIcon />
+            </IconButton>
+            </div>
+        </div>
         <div className='row' style={{ margin:'25px' }}>
           {items.map((item) => (
             <div key={item.id} className='col' style={{ textAlign:'center' }}>
                 <div >
-              <Card className='category-card' style={{backgroundColor: 'rgba(255, 255, 255, 0.301)', width: '35rem' }}>
+              <Card className='category-card' style={{backgroundColor: 'rgba(255, 255, 255, 0.301)', width: '20rem' }}>
               <Card.Img variant="top" src={item.banner_img} />
                 <Card.Body>
                     <div className='row'>
@@ -67,7 +202,16 @@ function ItemPage() {
                             <h5>Price: RS:{item.price}</h5>
                         </div>
                     </div>
-                  
+                    <div className='row'>
+                        <div className='col' style={{ textAlign:'center' }}>
+                        <IconButton onClick={() => handleEditItem(item)}>
+                          <EditIcon />
+                        </IconButton>
+                        <IconButton onClick={() => handleDeleteItem(item.id)}>
+                          <DeleteIcon />
+                        </IconButton>
+                        </div>
+                    </div>
                 </Card.Body>
               </Card>
             </div>
@@ -75,6 +219,42 @@ function ItemPage() {
           ))}
         </div>
       </div>
+      {/* success  Popup */}
+      {isSuccessPopupOpen && (
+        <SuccessComponent
+          message="Successfully added new item"
+          closeSuccessPopup={closeSuccessPopup}
+        />
+      )}
+
+      {/*  Add Popup */}
+      {isAddPopupOpen && (
+        <AddItemPopup
+          add={add}
+          closePopup={closeAddPopup}
+         
+        />
+      )}
+
+      {/* Delete item Popup */}
+      {isDeletePopupOpen && (
+        <DeletePopup
+          confirmDelete={confirmDelete}
+          closePopup={() => setDeletePopupOpen(false)}
+          message="Are you sure, Do you want to delete this Item?"
+        />
+      )}
+{/* update item Popup */}
+{isUpdatePopupOpen && itemToUpdate && (
+        <UpdateItemPopup
+          data={itemToUpdate}
+          update={update}
+          closePopup={closeUpdatePopup}
+          
+        />
+      )}
+   
+    
     </div>
   );
 }
--- src/pages/ItemsPage.js
+++ src/pages/ItemsPage.js
@@ -237,7 +237,7 @@ const confirmDelete = () => {
         <AddItemPopup
           add={add}
           closePopup={closeAddPopup}
-          name="Type"
+          
         />
       )}
 
